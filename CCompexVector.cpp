086b9d7a22a9e155a9a4fc40b34d63875363eee1
philipkarev <filipp.karev@math.msu.ru>
2023-02-18 20:08:36 +0300

    reload some operators

--- CCompexVector.cpp
+++ CCompexVector.cpp
@@ -1,10 +1,10 @@
 #include "CCompexVector.h"
 
-CCV::CCV(compex* a, int len) {
+CCV::CCV(compex *a, int len) {
 
-    this -> len = len;
+    this->len = len;
 
-    brr = new compex[len]; 
+    brr = new compex[len];
     for (int i = 0; i < len; i++) {
         brr[i] = a[i];
     }
@@ -18,123 +18,148 @@ CCV::~CCV() {
 }
 
 
-CCV::CCV(const CCV& b) {
+CCV::CCV(const CCV &b) {
 
-    this -> len = b.len;
+    this->len = b.len;
 
     brr = new compex[len];
     for (int i = 0; i < len; i++) {
-        this -> brr[i] = b.brr[i];
+        this->brr[i] = b.brr[i];
     }
 }
 
 
-CCV::CCV(CCV&& obj) noexcept {
+CCV::CCV(CCV &&obj) noexcept {
 
-    brr = obj.brr;
-    len = obj.len;
-    obj.len = 0;
+brr = obj.brr;
+len = obj.len;
+obj.
+len = 0;
 }
 
 
-CCV& CCV::operator=(const CCV& b) {
+CCV &CCV::operator=(const CCV &b) {
 
-    this -> len = b.len;
+    this->len = b.len;
 
     brr = new compex[len];
     for (int i = 0; i < len; i++)
-        this -> brr[i] = b.brr[i];
+        this->brr[i] = b.brr[i];
     return *this;
 }
 
 
-CCV& CCV::operator=(CCV&& obj) noexcept {
+CCV &CCV::operator=(CCV &&obj)
 
-    if (&obj == this)
-        return *this;
-    delete[] brr;
-    len = obj.len;
-    brr = obj.brr;
-    obj.len = 0;
-    return *this;
+noexcept {
+
+if (&obj == this)
+return *this;
+delete[]
+brr;
+len = obj.len;
+brr = obj.brr;
+obj.
+len = 0;
+return *this;
 }
 
 
-CCV CCV::operator+(const CCV& a) const {
+CCV CCV::operator+(const CCV &a) const {
 
-    int l = (a.len > this -> len ? this -> len : a.len);
+    int l = (a.len > this->len ? this->len : a.len);
     CCV crr = CCV(a.brr, l);
     for (int i = 0; i < l; i++)
-        crr.brr[i] = this -> brr[i] + a.brr[i];
+        crr.brr[i] = this->brr[i] + a.brr[i];
     return crr;
 }
 
 
-CCV CCV::operator-(const CCV& a) const {
+CCV CCV::operator-(const CCV &a) const {
 
-    int l = (a.len > this -> len ? this -> len : a.len);
+    int l = (a.len > this->len ? this->len : a.len);
     CCV crr = CCV(a.brr, l);
     for (int i = 0; i < l; i++)
-        crr.brr[i] = this -> brr[i] - a.brr[i];
+        crr.brr[i] = this->brr[i] - a.brr[i];
     return crr;
 }
 
 
-compex CCV::operator*(const CCV& a) const { // scalar product
+compex CCV::operator*(const CCV &a) const { // scalar product
 
     compex r(0, 0); // result of scalar product
 
-    for (int i = 0; i < (a.len > this -> len ? this -> len : a.len); ++i) // evaluate scalar product at the beginning
-        r = r + this -> brr[i] * a.brr[i];
+    for (int i = 0; i < (a.len > this->len ? this->len : a.len); ++i) // evaluate scalar product at the beginning
+        r = r + this->brr[i] * a.brr[i];
 
     return r;
 }
 
 
-CCV& CCV::operator++() {
+CCV &CCV::operator++() { // prefix
 
-    compex* crr = new compex[len+1];
+    compex *crr = new compex[len + 1];
+    crr[0] = (this->brr)[0];
     for (int i = 0; i < len; ++i)
-        crr[i] = brr[i];
-    crr[len] = crr[len-1];
+        crr[i + 1] = (this->brr)[i];
     delete[] brr;
     ++len;
-    this -> brr = crr;
+    this->brr = crr;
     return *this;
 }
 
 
-CCV& CCV::operator--() {
+/*CCV &CCV::operator--() { // prefix
 
-    if (len > 1)
-        --len;
+    if (len > 1) {
+        compex *crr = new compex[len - 1];
+        for (int i = 0; i < len; ++i)
+            crr[i] = (this->brr)[i];
+        delete[] brr;
+        ++len;
+        this->brr = crr;
+        return *this;
+    }
     else
         cout << "\nError: incorrect data. Decrement operation is unavailable" << endl;
     return *this;
+}*/
+
+
+CCV CCV::operator++(int) { // postfix
+
+    CCV tmp(*this);
+
+    compex *crr = new compex[tmp.len + 1];
+    for (int i = 0; i < tmp.len; ++i)
+        crr[i] = tmp.brr[i];
+    crr[tmp.len] = tmp.brr[tmp.len - 1];
+
+    CCV res(crr, tmp.len + 1);
+
+    return res;
 }
 
 
-CCV CCV::operator++(int) {
+CCV CCV::operator--(int) { // postfix
 
     CCV tmp(*this);
-    ++(*this);
+    if (tmp.len > 1) {
+        (tmp.len)--;
+    }
+    else {
+        cout << "\nError: incorrect data. Decrement operation is unavailable" << endl;
+    }
+
     return tmp;
 }
 
 
-CCV CCV::operator--(int) {
-
-    CCV tmp(*this);
-    --(*this);
-    return tmp;
-}
-
-
-ostream& operator << (ostream &stream, const CCV& s) {
+ostream &operator<<(ostream &stream, const CCV &s) {
 
     stream << "[";
     for (int i = 0; i < s.len; i++) {
-        if ( i != 0 ) {
+        if (i != 0) {
             stream << ", ";
         }
         stream << s.brr[i];
